80b203b2566310b63e65801edd5886f6340ae3dc
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-05-19 20:59:54 +0100

    Made database and routes more realistic

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 
-// Database
+// Database Setup
 var fs = require("fs");
 var file = "test.db";
 var exists = fs.existsSync(file);
 
+// Create db file if it doesn't exist
 if(!exists) {
     console.log("Creating DB file.");
     fs.openSync(file, "w");
@@ -14,39 +15,49 @@ if(!exists) {
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
 
+// If we created a db before, need to create the tables now
+var TABLES = [
+    "CREATE TABLE Coffees (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, date DATETIME DEFAULT CURRENT_TIMESTAMP, unit_price DECIMAL(5,2))",
+    "CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, card_id VARCHAR(255), name VARCHAR(255))",
+    "CREATE TABLE Payments (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, date DATETIME DEFAULT CURRENT_TIMESTAMP, amount DECIMAL(5,2))"
+];
+
 db.serialize(function() {
     if(!exists) {
-        db.run("CREATE TABLE Coffees (thing TEXT)");
+        for(var i = 0; i < TABLES.length; i++)
+            db.run(TABLES[i]);
     }
 });
 
-app.get('/test', function (req, res) {
-    var c = doSelect("SELECT * FROM Coffees");
-    console.log(c);
-    res.send(c);
+app.get('/users/:user_id/coffees', function (req, res) {
+    var user_id = parseInt(req.params.user_id);
+    if(!isNaN(user_id) && user_id > 0) {
+        doSelect("SELECT * FROM Coffees WHERE user_id = " + user_id, function (coffeeList) {
+            console.log(coffeeList);
+            res.status(200).send(coffeeList);
+        });
+    } else {
+        res.status(404).send('User not found with ID: ' + req.params.user_id);
+    }
 });
 
-app.post('/test', function (req, res) {
-    doInsert("INSERT INTO Coffees VALUES (?)", ['Test']);
-    res.send('OK');
+app.post('/users/:user_id/coffees', function (req, res) {
+    var user_id = parseInt(req.params.user_id);
+    if(!isNaN(user_id) && user_id > 0) {
+        doInsert("INSERT INTO Coffees(id, user_id, unit_price) VALUES (NULL, ?, ?)", [[user_id, 0.30]]);
+        console.log("Inserted 1 Coffee into database for user with ID: " + user_id);
+        res.status(201).send('OK');
+    } else {
+        res.status(404).send('User not found with ID: ' + req.params.user_id);
+    }
 });
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
-function doQuery(queryFunction, callbackFunction) {
-    var output;
-
-    db.serialize(function() {
-        output = queryFunction(db, output);
-    });
-
-    return output;
-}
-
 function doInsert(statement, values) {
-    doQuery(function (db) {
+    db.serialize(function() {
         var stmt = db.prepare(statement);
         for (var i = 0; i < values.length; i++) {
             stmt.run(values[i]);
@@ -55,17 +66,17 @@ function doInsert(statement, values) {
     });
 }
 
-function doSelect(query) {
-    doQuery(function (db, output) {
+function doSelect(query, callback) {
+    var output = [];
+
+    db.serialize(function() {
         db.each(query,
             function(err, row) {
                 output.push(row);
             },
-            function(err, rows) {
-                console.log(rows);
-                return output;
+            function (err, rowCount) {
+                callback(output);
             }
         );
     });
 }
-
