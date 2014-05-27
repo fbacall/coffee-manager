4f45ad42b9c3eb374bbfe7e0ef81ee3bf8db3553
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-05-27 14:43:35 +0100

    Serve the client through the Node server!

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 'use strict';
 var express = require('express');
 var app = express();
+
 app.use(express.json()); // To parse json in POST requests
+app.use(express.static(__dirname + '/../client')); // For serving the client pages
+
 var db = require('./database')();
 
 require('./app')(app, db);
